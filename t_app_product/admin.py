febac4eeff64686fdb9c5eb72df5fa82ae7070df
Mario Roberto <mrgomez.dev@gmail.com>
2024-04-07 00:45:19 -0600

    Task Model

--- t_app_product/admin.py
+++ t_app_product/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product
+
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ("created", )
 
 # Register your models here.
+admin.site.register(Product, ProductAdmin)
--- t_app_product/models.py
+++ t_app_product/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=None)
+    
+    def __str__(self):
+        return self.title + ' - by ' +self.user.username
+    
