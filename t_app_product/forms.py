180557dde5b38d1ee9baebd91929498febf8d344
Mario Roberto <mrgomez.dev@gmail.com>
2024-04-20 16:13:55 -0600

    Add performance style to template in boostrap

--- t_app_product/forms.py
+++ t_app_product/forms.py
@@ -1,7 +1,15 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Product
 
-class ProductForm(ModelForm):
+class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['title', 'description', 'price', 'category', 'important', 'img']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}), # Utilizo NumberInput para el campo DecimalField 'price'
+            'category': forms.TextInput(attrs={'class': 'form-control'}),
+            'important': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'img': forms.FileInput(attrs={'class': 'form-control-file'}), # Utilizo FileInput para el campo ImageField 'img'
+        }
