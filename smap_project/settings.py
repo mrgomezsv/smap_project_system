2cb761b267d61fa83fc8dfc0824ecb20a5967153
Mario Roberto <mrgomez.dev@gmail.com>
2024-04-26 23:12:07 +0000

    DESPLIEGUE EN SERVIDOR OK

--- smap_project/settings.py
+++ smap_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(y@^qkirxh^6wd9#913ts$a!3j@!gfrnsv-lj@_%$+%$iml*k2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['smap.webcraftedsolutions.net']
 
 
 # Application definition
@@ -119,7 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Definir las rutas adicionales para buscar archivos estáticos
+STATICFILES_DIRS = [
+    BASE_DIR / "t_app_product" / "static",
+]
 
 LOGIN_URL = '/signin'
 
@@ -132,3 +137,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuración adicional para CSRF
+CSRF_TRUSTED_ORIGINS = ['https://smap.webcraftedsolutions.net']
+CSRF_COOKIE_SECURE = True
+
--- smap_project/urls.py
+++ smap_project/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for smap_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from t_app_product import views
@@ -28,12 +12,13 @@ urlpatterns = [
     path('logout/', views.signout, name='logout'),
     path('signin/', views.signin, name='signin'),
     path('product/create/', views.create_product, name='create_product'),
-    #path('product/<int:product_id>/', views.product_detail, name='product_detail'),
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
     path('product/<int:product_id>/delete', views.delete_product, name='delete_product'),
 ]
 
+# Sirve las imágenes desde la carpeta media
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-#Sirve las imagenes desde la carpeta media solo en DEBUG
+# Sirve los archivos estáticos durante el desarrollo
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
