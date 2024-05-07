2b792cf1c3fa3ae97fafbb0bd191e137c327927a
Mario Roberto <mrgomez.dev@gmail.com>
2024-05-07 10:45:59 -0600

    Add template services and contact

--- kidsfun_web/urls.py
+++ kidsfun_web/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name='home'),  # Define la URL para la página de inicio
-    path('products-and-services/', views.products_and_services_view, name='products_and_services'),
-    path('contact/', views.contact_view, name='contact'),
-    # Aquí puedes agregar más rutas según las vistas que tengas en tu aplicación
+    path('', views.home_view, name='home'),
+    path('services/', views.services, name='services'),
+    path('contact/', views.contact, name='contact'),
+    # Otras rutas si las tienes
 ]
--- kidsfun_web/views.py
+++ kidsfun_web/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 
 def home_view(request):
-    # Lógica para renderizar la página de inicio
-    return render(request, 'home.html')  # Asegúrate de tener una plantilla llamada 'home.html' en la carpeta 'templates/kidsfun_web/'
+    return render(request, 'home.html')
 
 
-def products_and_services_view(request):
-    return render(request, 'products_and_services.html')
+def services(request):
+    return render(request, 'services.html')
 
-def contact_view(request):
+def contact(request):
     return render(request, 'contact.html')
