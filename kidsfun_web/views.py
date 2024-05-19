2bba4dec0fd065e546802c640a209086931c179f
Mario Roberto <mrgomez.dev@gmail.com>
2024-05-18 21:31:38 -0600

    Add page service

--- kidsfun_web/views.py
+++ kidsfun_web/views.py
@@ -1,5 +1,10 @@
 # En kidsfun_web/views.py
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, 'kidsfun_web/home.html')
+
+
+def service(request):
+    return render(request, 'kidsfun_web/service.html')
