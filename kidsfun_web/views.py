50c6bc090648cde943ae7a4b65409562a3247456
Mario Roberto <mrgomez.dev@outlook.com>
2024-05-24 02:03:27 -0600

    Data ok in service html

--- kidsfun_web/views.py
+++ kidsfun_web/views.py
@@ -9,6 +9,12 @@ def home(request):
 
 def service(request):
     productos = Product.objects.all()  # Obtén todos los productos de la base de datos
+
+    # Imprimir los datos de los productos en la terminal
+    for producto in productos:
+        print(
+            f"Nombre: {producto.title}, Descripción: {producto.description}, Precio: {producto.price}, Categoría: {producto.category}, Dimensiones: {producto.dimensions}, Usuario: {producto.user.username}")
+
     return render(request, 'kidsfun_web/service/service.html', {'productos': productos})
 
 
