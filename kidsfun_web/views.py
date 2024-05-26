cb32707150653d5cbc23773bd6b14909d6b72364
Mario Roberto <mrgomez.dev@gmail.com>
2024-05-26 13:46:52 -0600

    Add filter in views product kidsfun web for publicated

--- kidsfun_web/views.py
+++ kidsfun_web/views.py
@@ -1,14 +1,13 @@
 # En kidsfun_web/views.py
 from django.shortcuts import render
-from t_app_product.models import Product # Importa tu modelo de Producto
-
+from t_app_product.models import Product  # Importa tu modelo de Producto
 
 def home(request):
     return render(request, 'kidsfun_web/home/home.html')
 
-
 def service(request):
-    productos = Product.objects.all()  # Obtén todos los productos de la base de datos
+    # Obtén solo los productos que están publicados
+    productos = Product.objects.filter(publicated=True)
 
     # Imprimir los datos de los productos en la terminal
     for producto in productos:
@@ -16,7 +15,6 @@ def service(request):
             f"Nombre: {producto.title}, Descripción: {producto.description}, Precio: {producto.price}, Categoría: {producto.category}, Dimensiones: {producto.dimensions}, Usuario: {producto.user.username}")
 
     return render(request, 'kidsfun_web/service/service.html', {'productos': productos})
-
 
 def contact(request):
     return render(request, 'kidsfun_web/contact/contact.html')
