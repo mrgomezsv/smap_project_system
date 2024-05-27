dddd7c3d1251232255ad30ff012963d48c1d92de
Mario Roberto <mrgomez.dev@gmail.com>
2024-05-26 18:03:33 -0600

    performance code

--- kidsfun_web/views.py
+++ kidsfun_web/views.py
@@ -2,8 +2,10 @@
 from django.shortcuts import render
 from t_app_product.models import Product  # Importa tu modelo de product
 
+
 def home(request):
     return render(request, 'kidsfun_web/home/home.html')
+
 
 def service(request):
     # Obtener todos los products que están publicados
@@ -12,12 +14,16 @@ def service(request):
     # Crear un diccionario para almacenar los products agrupados por categoría
     products_or_category = {}
     for product in products:
+        # print(
+        #     f"Nombre: {product.title}, Descripción: {product.description}, Precio: {product.price}, "
+        #     f"Categoría: {product.category}, Dimensiones: {product.dimensions}, Usuario: {product.user.username}")
         category = product.category
         if category not in products_or_category:
             products_or_category[category] = []
         products_or_category[category].append(product)
 
     return render(request, 'kidsfun_web/service/service.html', {'products_or_category': products_or_category})
+
 
 def contact(request):
     return render(request, 'kidsfun_web/contact/contact.html')
