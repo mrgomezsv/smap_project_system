a1e676a1835460c2e5772d5684218c39121961f5
Mario Roberto <mrgomez.dev@gmail.com>
2024-05-26 19:28:24 -0600

    Gets the product information and takes it to the template where it will be presented

--- kidsfun_web/urls.py
+++ kidsfun_web/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('service/', views.service, name='service/service'),
-    path('service/product/', views.service_product, name='service_product'),
+    path('service/', views.service, name='service'),
+    path('service/product/<int:product_id>/', views.service_product, name='service_product'),
+    path('contact/', views.contact, name='contact'),
     # path('contact/', views.service, name='contact/contact'),
 ]
--- kidsfun_web/views.py
+++ kidsfun_web/views.py
@@ -1,5 +1,5 @@
 # En kidsfun_web/views.py
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from t_app_product.models import Product  # Importa tu modelo de product
 
 
@@ -10,7 +10,6 @@ def home(request):
 def service(request):
     # Obtener todos los products que están publicados
     products = Product.objects.filter(publicated=True)
-
     # Crear un diccionario para almacenar los products agrupados por categoría
     products_or_category = {}
     for product in products:
@@ -21,12 +20,25 @@ def service(request):
         if category not in products_or_category:
             products_or_category[category] = []
         products_or_category[category].append(product)
-
     return render(request, 'kidsfun_web/service/service.html', {'products_or_category': products_or_category})
 
 
-def service_product(request):
-    return render(request, 'kidsfun_web/service/product/product.html')
+def service_product(request, product_id):
+    # Obtener el producto correspondiente al product_id o mostrar una página 404 si no existe
+    product = get_object_or_404(Product, pk=product_id)
+
+    # Imprimir los datos del producto en la consola
+    # print("Datos del producto:")
+    # print("ID:", product.id)
+    # print("Título:", product.title)
+    # print("Descripción:", product.description)
+    # print("Categoria:", product.category)
+    # print("Dimensiones:", product.dimensions)
+    # print("Usiario:", product.user.username)
+    # Continúa imprimiendo cualquier otro dato que desees
+
+    # Devolver la renderización de la plantilla con el producto
+    return render(request, 'kidsfun_web/service/product/product.html', {'product': product})
 
 
 def contact(request):
