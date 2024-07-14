07427e044d2bef362a13861c6103630f7da26f30
Mario Roberto <mrgomez.dev@gmail.com>
2024-07-14 16:41:04 -0600

    Creation models waiverqr

--- api_waiver/models.py
+++ api_waiver/models.py
@@ -13,6 +13,17 @@ class WaiverData(models.Model):
         return f"{self.user_name}'s waiver data for {self.relative_name}"
 
 
-
     class Meta:
         db_table = 'api_waiver_waiverdata'
+
+
+class WaiverQR(models.Model):
+    waiver_data = models.OneToOneField(WaiverData, on_delete=models.CASCADE, related_name='qr_data')
+    qr_value = models.CharField(max_length=100, unique=True)
+
+    def save(self, *args, **kwargs):
+        self.qr_value = f"{self.waiver_data.user_id}{self.waiver_data.timestamp.strftime('%Y%m%d%H%M%S%f')}"
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.qr_value
