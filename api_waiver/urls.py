fb1547f143bbda7f8fcb2a8a81aa86b9fc85dfe3
Mario Roberto <mrgomez.dev@gmail.com>
2024-07-13 21:18:09 -0600

    REFACTOR URL API WAIVER

--- api_waiver/urls.py
+++ api_waiver/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api_waiver/', views.api_waiver, name='api_waiver'),
+    path('waiver/', views.api_waiver, name='api_waiver'),
 ]
