7bc7e26648047524a320de0d544f0b9f6b956dfc
Mario Roberto <mrgomez.dev@gmail.com>
2024-09-08 15:25:04 -0600

    Eliminada la ruta 'waiver_qr/' en 'urls.py' debido a error de vista no definida. Funcionalidad combinada en 'api_waiver' para simplificación y corrección de errores.

--- api_waiver/urls.py
+++ api_waiver/urls.py
@@ -5,6 +5,5 @@ from . import views
 
 urlpatterns = [
     path('waiver/', views.api_waiver, name='api_waiver'),
-    path('waiver_qr/', views.api_waiver_qr, name='api_waiver_qr'),
-    path('waiver_qr/<str:user_id>/', views.get_waiver_data, name='get_waiver_data'),  # Añadir esta línea
+    path('waiver_qr/<str:user_id>/', views.get_waiver_data, name='get_waiver_data'),
 ]
