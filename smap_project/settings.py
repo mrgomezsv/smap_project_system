68dd399ba45b650ee43458a3af642382c6f7844b
Mario Roberto <mrgomez.dev@gmail.com>
2024-10-27 19:39:48 -0600

    Probando

--- smap_project/settings.py
+++ smap_project/settings.py
@@ -108,17 +108,28 @@ WSGI_APPLICATION = 'smap_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'smap_kf',
+#         'USER': 'mrgomez',
+#         'PASSWORD': 'Karin2100',
+#         'HOST': '82.165.210.146',
+#         'PORT': '5432',
+#         'OPTIONS': {
+#                     'sslmode': 'require',  # o 'prefer' si permites conexiones sin SSL también
+#                 },
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'smap_kf',
         'USER': 'mrgomez',
         'PASSWORD': 'Karin2100',
-        'HOST': '82.165.210.146',
+        'HOST': 'localhost',
         'PORT': '5432',
-        'OPTIONS': {
-                    'sslmode': 'require',  # o 'prefer' si permites conexiones sin SSL también
-                },
     }
 }
 
