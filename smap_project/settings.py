f40228325959fa132ee6aa177348979cb3085646
Mario Roberto <mrgomez.dev@gmail.com>
2024-10-28 10:25:38 +0000

    Recoleccion y configuracion de archivos estaticos en produccion

--- smap_project/settings.py
+++ smap_project/settings.py
@@ -166,18 +166,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Definir las rutas adicionales para buscar archivos estáticos
 STATICFILES_DIRS = [
+    BASE_DIR / "static",
     BASE_DIR / "t_app_product" / "static",
 ]
 
 LOGIN_URL = '/signin'
 
 MEDIA_URL = '/media/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
