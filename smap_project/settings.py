8446dbd58ca0db58621e1a63ab4615c1677abd1a
Mario Roberto <mrgomez.dev@gmail.com>
2024-11-16 17:34:54 -0600

    Refactor MEDIA_ROOT para guardar archivos en produccion

--- smap_project/settings.py
+++ smap_project/settings.py
@@ -177,7 +177,8 @@ STATICFILES_DIRS = [
 LOGIN_URL = '/signin'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, '/root/smap_project_system/media/')
+MEDIA_ROOT = '/root/smap_project_system/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
