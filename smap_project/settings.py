a046fccebdf1462618541f4f1b6ac3da5242cafe
Mario Roberto <mrgomez.dev@gmail.com>
2025-01-21 06:17:16 +0000

    Update SETTINGS for deploy in production new

--- smap_project/settings.py
+++ smap_project/settings.py
@@ -40,7 +40,6 @@ cred_path = os.path.join(BASE_DIR, 'credentials', 'smap-kf-firebase-adminsdk-xqq
 cred = credentials.Certificate(cred_path)
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -118,22 +117,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'smap_project.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'smap_kf',
-#         'USER': 'mrgomez',
-#         'PASSWORD': 'Karin2100',
-#         'HOST': '82.165.210.146',
-#         'PORT': '5432',
-#         'OPTIONS': {
-#                     'sslmode': 'require',  # o 'prefer' si permites conexiones sin SSL también
-#                 },
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -175,10 +158,11 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/var/www/smap_project_system/staticfiles'
+
+
 
 # Definir las rutas adicionales para buscar archivos estáticos
 STATICFILES_DIRS = [
@@ -187,13 +171,6 @@ STATICFILES_DIRS = [
 ]
 
 LOGIN_URL = '/signin'
-
-MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, '/root/smap_project_system/media/')
-MEDIA_ROOT = '/root/smap_project_system/media/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
