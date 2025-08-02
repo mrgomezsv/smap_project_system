e6d7eee9a46b1aa4e011e703a0ccd394cc8d6d80
Mario Roberto <gomez@thetecwave.com>
2025-08-01 23:54:30 -0600

    Corregir importación del modelo Product en kidsfun_web
    - Cambiar importación de api.models.Product a t_app_product.models.Product
    - El modelo correcto tiene el campo publicated que se necesita
    - Corregir error de campo 'publicado' no encontrado
    - Asegurar que se use el modelo correcto con todos los campos necesarios

--- kidsfun_web/views.py
+++ kidsfun_web/views.py
@@ -6,7 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 import json
 
-from api.models import Product
+from t_app_product.models import Product
 from api_like.models import Like
 from api_commentary.models import Commentary
 
